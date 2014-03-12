11c46768dced567bce934c7915507d95d425e751
michael <mcorso@ycp.edu>
2014-03-12 15:50:53 -0400

    add friendsList

---
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/FriendsList.java
@@ -0,0 +1,15 @@
+package edu.ycp.cs320.awesomepage.shared;
+
+public class FriendsList {
+	private String friendName;
+
+	public String getFriendName() {
+		return friendName;
+	}
+
+	public void setFriendName(String friendName) {
+		this.friendName = friendName;
+	}
+	
+
+}
