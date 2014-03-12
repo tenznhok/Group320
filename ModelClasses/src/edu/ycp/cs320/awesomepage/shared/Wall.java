cd30a02263e221fd3e6f455c124b20cb959d2755
ThuongNguyen <tnguye17@ycp.edu>
2014-03-12 15:42:57 -0400

    add wall

---
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/Wall.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs320.awesomepage.shared;
+
+public class Wall {
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
