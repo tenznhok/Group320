c73d63daf45b13459165985a0fbf291e6ef29ae9
ThuongNguyen <tnguye17@ycp.edu>
2014-03-12 15:15:22 -0400

    user class

---
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/User.java
@@ -0,0 +1,17 @@
+package edu.ycp.cs320.awesomepage.shared;
+
+public class User {
+	private String userName;
+	
+	public User() {
+		
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+}
