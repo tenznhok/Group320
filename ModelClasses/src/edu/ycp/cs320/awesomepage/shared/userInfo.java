8862c2ead81aae98f897ed48b58ea620be257284
Zackeree <zhenry@ycp.edu>
2014-03-19 11:15:17 -0400

    Update userInfo.java
    Improved readability
    Added fields for location(country, city)

--- ModelClasses/src/edu/ycp/cs320/awesomepage/shared/userInfo.java
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/userInfo.java
@@ -6,43 +6,73 @@ public class userInfo {
 	private String emailContact;
 	private String maleOrFemale;
 	private String phoneNum;
+	private String country;
+	private String city;
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getEmailContact() {
 		return emailContact;
 	}
+	
 	public void setEmailContact(String emailContact) {
 		this.emailContact = emailContact;
 	}
+	
 	public String getMaleOrFemale() {
 		return maleOrFemale;
 	}
+	
 	public void setMaleOrFemale(String maleOrFemale) {
 		this.maleOrFemale = maleOrFemale;
 	}
+	
 	public String getPhoneNum() {
 		return phoneNum;
 	}
+	
 	public void setPhoneNum(String phoneNum) {
 		this.phoneNum = phoneNum;
 	}
 	
-	public void editInfo(String firstName, String lastName, String emailContact, String maleOrFemale, String phoneNum){
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void editInfo(String firstName, String lastName, String emailContact, String maleOrFemale, String phoneNum, String country, String city){
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailContact = emailContact;
 		this.maleOrFemale = maleOrFemale;
 		this.phoneNum = phoneNum;
+		this.country = country;
+		this.city = city;
 	}
 	
 
