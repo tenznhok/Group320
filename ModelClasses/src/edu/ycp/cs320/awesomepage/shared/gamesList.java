3ea0c43a65688f758e92503ee3665d7ad1207f0d
ThuongNguyen <tnguye17@ycp.edu>
2014-03-21 11:23:48 -0400

    initial checkin of gamesList class

---
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/gamesList.java
@@ -0,0 +1,45 @@
+package edu.ycp.cs320.awesomepage.shared;
+
+import java.util.ArrayList;
+
+public class gamesList {
+	private games gameName;
+	private ArrayList<games> listOfGames;
+	
+
+	// add games into the user's list
+	public void addFriend(ArrayList<games> games){
+		listOfGames.addAll(games);		
+	}
+	
+	//remove games from the user's list
+	public void removeFriend(games games){
+		ArrayList<games> toRemove = new ArrayList<games>();
+		
+			for (int i = 0; i < listOfGames.size(); i++){
+				games = listOfGames.get(i);
+				if (games.equals(getGameName())){
+					toRemove.add(games);
+				}
+			}
+			
+			listOfGames.removeAll(toRemove);
+	}
+	
+	
+	//count how many games in the list
+	public int countFriend(){
+		int count; 
+		count = listOfGames.size();
+		return count;
+	}
+
+	public games getGameName() {
+		return gameName;
+	}
+
+	public void setGameName(games gameName) {
+		this.gameName = gameName;
+	}
+	
+}
