47faffab2ae9bfd1d8f0f0e229ddf26b6cb66308
michael <mcorso@ycp.edu>
2014-03-21 12:17:59 -0400

    changes to user page

--- ModelClasses/src/edu/ycp/cs320/awesomepage/shared/userPage.java
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/userPage.java
@@ -1,5 +1,20 @@
 package edu.ycp.cs320.awesomepage.shared;
 
 public class userPage {
-
+	
+	private Wall userWall = new Wall();
+	private Status userStatus = new Status();
+	private FriendsList userFriends = new FriendsList();
+	
+	
+	public void upDateStatus( String message ) {
+		
+		userStatus.setMessage(message);
+		
+	}
+	
+	public String getStatus()
+	{
+		return userWall.getStatus();
+	}
 }
