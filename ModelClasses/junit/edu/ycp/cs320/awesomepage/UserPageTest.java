8b32a89ec8ebf6bef9f27a6ca64d4ce4ef53d524
michael <mcorso@ycp.edu>
2014-03-26 12:40:58 -0400

    Started Test For User Page

--- ModelClasses/junit/edu/ycp/cs320/awesomepage/UserPageTest.java
+++ ModelClasses/junit/edu/ycp/cs320/awesomepage/UserPageTest.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.ycp.cs320.awesomepage.shared.userPage;
+
 public class UserPageTest {
+	
+	private userPage page = new userPage();
+	
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetStatus() {
+		
 	}
 
 }
