0342d719933bc9b6e97a94bafbe0c5b000ec9f4a
Michael <mcorso@ycp.edu>
2014-03-27 20:35:22 -0400

    update to CreateTable

--- ModelClasses/src/dataBase/CreateTable.java
+++ ModelClasses/src/dataBase/CreateTable.java
@@ -10,32 +10,26 @@ public class CreateTable {
 	public static void main( String args[] ) throws SQLException{
 	    Connection c = null;
 	    Statement stmt = null;
+	    
 	    try {
 	      Class.forName("org.sqlite.JDBC");
 	      c = DriverManager.getConnection("jdbc:sqlite:users.db");
 	      System.out.println("Opened database successfully");
+	      
+	      stmt = c.createStatement();
+	      
+	      String sql = 
+					"	create table users " +
+					"	(id integer primary key NOT NULL," +
+					"	user_id integer NOT NULL," +
+					"	name_name varchar(25) )";
+			stmt.executeUpdate( sql );
+			stmt.close();
+			c.close();
 	    } catch ( Exception e ) {
 	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	      System.exit(0);
 	    }
-	    
-	    PreparedStatement stmt1 = null;
-	    
-	    try {
-			stmt1 = c.prepareStatement(
-					"create table users (" +
-					"    id integer primary key," +
-					"    user_id integer," +
-					"    name_name varchar(25)," +
-					")");
-			stmt1.executeUpdate();
-			stmt1.close();
-		} finally {
-		   //c.commit();
-		   c.close();
-		}
-	    
 	    System.out.println("Table created successfully");
 	  }
-	
 }
