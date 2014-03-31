9031436ca58a28b82a9157029a9fa5ac610972a7
michael <mcorso@ycp.edu>
2014-03-31 14:38:58 -0400

    add music, music test classes

--- ModelClasses/src/edu/ycp/cs320/awesomepage/shared/music.java
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/music.java
@@ -1,5 +1,16 @@
 package edu.ycp.cs320.awesomepage.shared;
 
 public class music {
+	
+	private String musicName;
+	
+	public void setMusicName( String name ){
+		
+		this.musicName = name;
+	}
+	public String getMusicName(  ){
+		
+		return this.musicName;
+	}
 
 }
