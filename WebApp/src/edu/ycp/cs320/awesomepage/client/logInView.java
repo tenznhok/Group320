bd69bff2e3a89401a50e9cb165df7b0a8ef89202
nhoktenz <nhoktenz@thuongs-mbp.home>
2014-04-04 10:47:14 -0400

    add logInView class. Try to do GWT.

---
+++ WebApp/src/edu/ycp/cs320/awesomepage/client/logInView.java
@@ -0,0 +1,15 @@
+package edu.ycp.cs320.awesomepage.client;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+
+public class logInView extends Composite {
+	
+
+}
