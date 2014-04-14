3b24917784101dbc2ec4027f04cf707e05ca8665
ThuongNguyen <tnguye17@ycp.edu>
2014-04-14 11:27:11 -0400

    signUp class, make switch back and forth in status classes and webpageClass

--- WebApp/src/edu/ycp/cs320/awesomepage/client/StatusPosting.java
+++ WebApp/src/edu/ycp/cs320/awesomepage/client/StatusPosting.java
@@ -9,8 +9,11 @@ import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class StatusPosting extends Composite {
+	private Button statusPostButt;
 	public StatusPosting() {
 		
 		LayoutPanel layoutPanel = new LayoutPanel();
@@ -22,9 +25,24 @@ public class StatusPosting extends Composite {
 		layoutPanel.setWidgetLeftWidth(statusTextbox, 0.0, Unit.PX, 483.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(statusTextbox, 54.0, Unit.PX, 128.0, Unit.PX);
 		
-		Button statusPostButt = new Button("Post");
+		statusPostButt = new Button("Post");
+		statusPostButt.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				postingStatus();
+			}
+
+			
+		});
 		layoutPanel.add(statusPostButt);
 		layoutPanel.setWidgetLeftWidth(statusPostButt, 14.0, Unit.PX, 61.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(statusPostButt, 198.0, Unit.PX, 30.0, Unit.PX);
+	}
+	
+	private void postingStatus() {
+		
+		
+		// Switch to webpage view
+			WebApp.setView(new webpageView());
+		
 	}
 }
---
+++ WebApp/src/edu/ycp/cs320/awesomepage/client/signUpView.java
@@ -0,0 +1,73 @@
+package edu.ycp.cs320.awesomepage.client;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Button;
+
+public class signUpView extends Composite {
+	public signUpView() {
+		
+		LayoutPanel layoutPanel = new LayoutPanel();
+		initWidget(layoutPanel);
+		layoutPanel.setHeight("385px");
+		
+		Label lblUsername = new Label("UserName :");
+		layoutPanel.add(lblUsername);
+		layoutPanel.setWidgetLeftWidth(lblUsername, 15.0, Unit.PX, 85.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(lblUsername, 73.0, Unit.PX, 18.0, Unit.PX);
+		
+		InlineLabel inlineLabel = new InlineLabel("First Name: ");
+		layoutPanel.add(inlineLabel);
+		layoutPanel.setWidgetLeftWidth(inlineLabel, 15.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(inlineLabel, 137.0, Unit.PX, 18.0, Unit.PX);
+		
+		InlineLabel nlnlblLastName = new InlineLabel("Last Name:");
+		layoutPanel.add(nlnlblLastName);
+		layoutPanel.setWidgetLeftWidth(nlnlblLastName, 10.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(nlnlblLastName, 177.0, Unit.PX, 18.0, Unit.PX);
+		
+		InlineLabel nlnlblPassword = new InlineLabel("Password:");
+		layoutPanel.add(nlnlblPassword);
+		layoutPanel.setWidgetLeftWidth(nlnlblPassword, 10.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(nlnlblPassword, 225.0, Unit.PX, 18.0, Unit.PX);
+		
+		InlineLabel nlnlblEmail = new InlineLabel("Email:");
+		layoutPanel.add(nlnlblEmail);
+		layoutPanel.setWidgetLeftWidth(nlnlblEmail, 15.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(nlnlblEmail, 268.0, Unit.PX, 18.0, Unit.PX);
+		
+		PasswordTextBox passwordTextBox = new PasswordTextBox();
+		layoutPanel.add(passwordTextBox);
+		layoutPanel.setWidgetLeftWidth(passwordTextBox, 123.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(passwordTextBox, 211.0, Unit.PX, 32.0, Unit.PX);
+		
+		TextBox textBox = new TextBox();
+		layoutPanel.add(textBox);
+		layoutPanel.setWidgetLeftWidth(textBox, 123.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox, 73.0, Unit.PX, 34.0, Unit.PX);
+		
+		TextBox textBox_1 = new TextBox();
+		layoutPanel.add(textBox_1);
+		layoutPanel.setWidgetLeftWidth(textBox_1, 123.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox_1, 121.0, Unit.PX, 34.0, Unit.PX);
+		
+		TextBox textBox_2 = new TextBox();
+		layoutPanel.add(textBox_2);
+		layoutPanel.setWidgetLeftWidth(textBox_2, 123.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox_2, 161.0, Unit.PX, 34.0, Unit.PX);
+		
+		TextBox textBox_3 = new TextBox();
+		layoutPanel.add(textBox_3);
+		layoutPanel.setWidgetLeftWidth(textBox_3, 121.0, Unit.PX, 163.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox_3, 252.0, Unit.PX, 34.0, Unit.PX);
+		
+		Button btnSignUp = new Button("Sign Up");
+		layoutPanel.add(btnSignUp);
+		layoutPanel.setWidgetLeftWidth(btnSignUp, 184.0, Unit.PX, 81.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(btnSignUp, 315.0, Unit.PX, 30.0, Unit.PX);
+	}
+}
