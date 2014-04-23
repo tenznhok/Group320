9acb89cfb9e72b7dd3d173a46f23a1a792df8e62
michael <mcorso@ycp.edu>
2014-04-23 12:07:12 -0400

    user infoooooo

--- Persistence/src/edu/ycp/cs320/awesomepage/server/model/persist/FakeDatabase.java
+++ Persistence/src/edu/ycp/cs320/awesomepage/server/model/persist/FakeDatabase.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.ycp.cs320.awesomepage.shared.User;
+import edu.ycp.cs320.awesomepage.shared.userInfo;
 
 public class FakeDatabase implements IDatabase {
 	private List<User> userList;
+	private List<userInfo> userInfoList;
 	
 	
 	public FakeDatabase() {
 		this.userList = new ArrayList<User>();
+		this.userInfoList = new ArrayList<userInfo>();
 		
 		// add initial user data
 		User user = new User();
@@ -19,6 +22,7 @@ public class FakeDatabase implements IDatabase {
 		user.setUserID(1);
 		user.setUserName("testuser");
 		userList.add(user);
+		userInfoStart( user.getUserID() );
 	}
 
 	@Override
@@ -31,7 +35,7 @@ public class FakeDatabase implements IDatabase {
 		}
 		return null;
 	}
-	
+	//sign up page, will add new user to database
 	@Override 
 	public User signUp( String userName, String password, String email ){
 		User newUser = new User();
@@ -41,6 +45,30 @@ public class FakeDatabase implements IDatabase {
 		newUser.setUserID(userList.size()+1);
 		userList.add(newUser);
 		
+		//makes empty user info
+		userInfoStart( newUser.getUserID() );
+		
 		return newUser;
 	}
+	//makes the users empty user info which than can be edited in the edit user info page
+	private void userInfoStart(int userID) 
+	{
+		userInfo newInfo = new userInfo();
+		newInfo.setId(userInfoList.size()+1);
+		newInfo.setUserId( userID );
+		newInfo.setFirstName(null);
+		newInfo.setLastName(null);
+		newInfo.setMaleOrFemale(null);
+		newInfo.setCity(null);
+		newInfo.setCountry(null);
+		newInfo.setPhoneNum(null);
+		
+		userInfoList.add(newInfo);
+	}
+
+	//@Override
+	//public userInfo editInfo( int userID )
+	//{
+		
+	//}
 }
