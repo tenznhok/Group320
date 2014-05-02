10d1dea958170672d2f2335f0546a9aed8517bbd
ThuongNguyen <tnguye17@ycp.edu>
2014-05-02 11:21:05 -0400

    2 more classes for userLists

---
+++ WebApp/src/edu/ycp/cs320/awesomepage/client/userListViewService.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs320.awesomepage.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import edu.ycp.cs320.awesomepage.shared.User;
+import edu.ycp.cs320.awesomepage.shared.userInfo;
+
+@RemoteServiceRelativePath("userListView")
+
+public interface userListViewService extends RemoteService{
+
+}
