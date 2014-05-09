fa3e77c68713119ccd24a8b0dd81be5383072204
michael <mcorso@ycp.edu>
2014-05-09 12:40:12 -0400

    made the code look nice and easy to read

--- ModelClasses/src/edu/ycp/cs320/awesomepage/shared/friendName.java
+++ ModelClasses/src/edu/ycp/cs320/awesomepage/shared/friendName.java
@@ -4,37 +4,15 @@ import java.io.Serializable;
 
 public class friendName implements Serializable {
 	private int friendID; // ID of each friend
-	
-	//private String friendFirstName;
-	//private String friendLastName;
 	private String userName;
-	
 	public friendName(){
 
-	
 	}
 	
 	public friendName( String userName, int friendID ){
-		//this.friendFirstName = friendFirstName;
-		//this.friendLastName = friendLastName;
 		this.friendID = friendID;
 		this.userName = userName;
-		
 	}
-	/*
-	public String getFriendFirstName() {
-		return friendFirstName;
-	}
-	public void setFriendFirstName(String friendFirstName) {
-		this.friendFirstName = friendFirstName;
-	}
-	public String getFriendLastName() {
-		return friendLastName;
-	}
-	public void setFriendLastName(String friendLastName) {
-		this.friendLastName = friendLastName;
-	}
-	*/
 	public int getFriendID() {
 		return friendID;
 	}
